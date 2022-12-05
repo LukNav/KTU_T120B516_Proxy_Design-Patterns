b01644a389c98a7f6dbff6dcec922cdf70f6ab81
LukNav <navasinskaslukas@gmail.com>
2022-12-05 23:11:21 +0200

    Add project files.

---
+++ Proxy/Controllers/ProxyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proxy.Controllers
+{
+    [Route("Proxy/Menu")]
+    [ApiController]
+    public class ProxyController : ControllerBase
+    {
+        
+    }
+}
